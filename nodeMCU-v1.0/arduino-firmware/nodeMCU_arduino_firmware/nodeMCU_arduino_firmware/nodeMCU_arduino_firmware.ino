2e8c1acce2197f65458926a394b4ba86af44e660
Kike.Ramirez <kike@vjspain.com>
2018-08-25 02:32:52 +0200

    Important cleaning... New face.

--- nodeMCU-v1.0/arduino-firmware/nodeMCU_arduino_firmware/nodeMCU_arduino_firmware/nodeMCU_arduino_firmware.ino
+++ nodeMCU-v1.0/arduino-firmware/nodeMCU_arduino_firmware/nodeMCU_arduino_firmware/nodeMCU_arduino_firmware.ino
@@ -19,174 +19,168 @@
 */
 
 
-#include <SPI.h>
-#include <Wire.h>
+// Library Import for OLED Display
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+
+// For touch Sensor
 #include <CapacitiveSensor.h>
+
+// For temperature & humidity sensor
+#include <DHT.h>
+// #include <DHT_U.h>
+
+// For main operations & Thinger.io processes
 #include <ESP8266WiFi.h>
 #include <ThingerESP8266.h>
 
-
+/*
+ * 
+ * PIN DEFINITIONS -----------> DHT, TOUCH & DISPLAY
+ * 
+ */
 #define DHTPIN            D6         // Pin which is connected to the DHT sensor.
-#define OLED_RESET LED_BUILTIN
 #define DHTTYPE           DHT22     // DHT 22 (AM2302)
+
 #define CAP_TX            D8
 #define CAP_RX            D7
+#define TOUCH_LEVEL       25.0
 
+#define OLED_RESET        LED_BUILTIN
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+// WIFI ACCESS PARAMETERS -----------> SSID & PASSWORD FOR WIFI ACCESS (NOT 5G)
+#define SSID "CarmenLauraKike"
+#define SSID_PASSWORD "Carmen2016"
 
-CapacitiveSensor   cs = CapacitiveSensor(CAP_TX,CAP_RX);        // 1Mohm resistor between pins 4 & 2, pin 2 is sensor pin
-
-
-uint32_t timerMeasure, timeMeasure;
-float temperature, humidity;
-
-Adafruit_SSD1306 display(OLED_RESET);
-
-boolean sleept = false;
-float timerSleep, timeSleep;
-
+// THINGER.IO -----------> USER, PWD & DEVICE CREDENTIALS, TOKEN, ETC.
 #define USERNAME "KikeRamirez"
 #define DEVICE_ID "NodeMCU"
 #define DEVICE_CREDENTIAL "YSqMCv9y%0Xo"
 
-#define SSID "CarmenLauraKike"
-#define SSID_PASSWORD "Carmen2016"
+// GENERAL PARAMETERS -----------> TIMINGS, FLAGS, DATA, ETC.
+#define TIMER_MEASURE 1000.0
+#define TIMER_SLEEP 5000.0
+#define VERBOSITY 1
 
+// GLOBAL VARIABLES -----------> TIMINGS, FLAGS, DATA, ETC.
+float temperature, humidity;
+float timeMeasure, timeSleep;
+
+// INITIALIZE OBJECTS FOR -----------> NODE IOT, DISPLAY, TOUCH SENSOR, TEMP&HUMID SENSOR.
+Adafruit_SSD1306 display(OLED_RESET);
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+DHT dht(DHTPIN, DHTTYPE);
+CapacitiveSensor cs = CapacitiveSensor(CAP_TX,CAP_RX);
 
-  
+
+/*
+ * 
+ * SETUP PROCESS -----------> INITIALIZE EVERYTHING AND SETS ZERO CONDITION
+ * 
+ */
+
 void setup()   {                
-  Serial.begin(9600);
-  Serial.println("Working");
 
-  cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - TOUCH SENSOR
-
-
-  dht.begin();
-  Serial.println("DHTxx Unified Sensor Example");
-  // Print temperature sensor details.
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.println("Temperature");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
-  Serial.println("------------------------------------");
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.println("Humidity");
-  Serial.print  ("Sensor Min Delay:       "); Serial.println(sensor.min_delay);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
-  Serial.println("------------------------------------");
+  if (VERBOSITY) {
+    Serial.begin(9600);
+    Serial.println("Starting...");
+  }
   
-  // Set delay between sensor readings based on sensor details.
-  timerMeasure = 1000.0;
-  timeMeasure = millis();
-
-  timerSleep = 5000.0;
-  timeSleep = millis();
+  // Initialize IOT Node and connect to WIFI
+  thing.add_wifi(SSID, SSID_PASSWORD);
   
-  temperature = 0.0;
-  humidity = 0.0;
+  // Declare output parameter "temperature"
+  thing["temperature"] >> [](pson& out){
+        out = dht.readTemperature();
+  };
 
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  // Declare output parameter "humidity"
+  thing["humidity"] >> [](pson& out){
+        out = dht.readHumidity();
+  };  
+  
+  // Initialize OLED Display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
-  // init done
   
   // Clear the buffer.
   display.clearDisplay();
 
-  
   // text display tests
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0,0);  
+  
+  // Initialize touch sensor
+  cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - TOUCH SENSOR
 
-  thing.add_wifi(SSID, SSID_PASSWORD);
-
-  thing["temperature"] >> [](pson& out){
-        out = temperature;
-  };
-
-  thing["humidity"] >> [](pson& out){
-        out = humidity;
-  };  
+  // Initialize temp & humidity sensor
+  dht.begin();    
 }
 
 
+/*
+ * 
+ * LOOP -----------> 
+ * 
+ */
+
 void loop() {
 
-  if (millis() - timeMeasure > timerMeasure) {
+  // Manage Thinger.io updates
+  thing.handle();
 
-    // Delay between measurements.
-    timeMeasure = millis();
-    
-    display.clearDisplay();
-  
-    display.setCursor(0,0);
-    
-    // Get temperature event and print its value.
-    sensors_event_t event;  
-    dht.temperature().getEvent(&event);
-    if (isnan(event.temperature)) {
-      Serial.println("Error reading temperature!");
-    }
-    else {
+  display.clearDisplay();  
 
-      temperature = event.temperature;
-    }
-    // Get humidity event and print its value.
-    dht.humidity().getEvent(&event);
-    if (isnan(event.relative_humidity)) {
-      Serial.println("Error reading humidity!");
-    }
-    else {
-      humidity = event.relative_humidity;
-    }
-
-    display.print("T: ");
-    display.print(temperature);
-    display.println(" C");
-
-    display.print("H: ");
-    display.print(humidity);
-    display.println("%");
-
-    display.display();
-    Serial.println("Measure done!");
-
-    thing.handle();
-
-
+  // If awake, update display
+  if (isAwake()) {
+    updateDisplay();
   }
 
-    long start = millis();
+  // Print logs
+  if (VERBOSITY) Serial.println("Measure done!");
 
-    long touch_measure =  cs.capacitiveSensor(30);
-//    Serial.print("Touch measure: ");
-//    Serial.println(touch_measure);
-//    Serial.print("Elapsed time: ");
-//    Serial.print(millis() - start);        // check on performance in milliseconds
-//    Serial.println(" ms.");
-//    Serial.println();
+}
 
-    delay(200);
+/*
+ * 
+ * CHECK TOUCH SENSOR -----------> UPDATES SLEEP MODE DEPENDING ON TOUCHES & TIMERS
+ * 
+ */
 
+bool isAwake() {
 
+  if (VERBOSITY) Serial.println("  -> Checking TOUCH SENSOR level");
+
+  // Updating touching status & timings
+  if ( cs.capacitiveSensor(30) > TOUCH_LEVEL) return true;
+  else return false;
+
+}
+
+/*
+ * 
+ * UPDATE DISPLAY -----------> UPDATES DISPLAY WITH NEW DATA, SPRITES, ANIMATIONS, ETC.
+ * 
+ */
+
+float updateDisplay() {
+
+      if (VERBOSITY) Serial.println("  -> Printing on OLED Display");
+
+      // Reset Position
+      display.setCursor(0,0);
+
+      // Print temperature
+      display.print("T: ");
+      display.print(dht.readTemperature());
+      display.println(" C");
+
+      // Print humidity
+      display.print("H: ");
+      display.print(dht.readHumidity());
+      display.println("%");
+
+      // Send to OLED
+      display.display();
 
 }
